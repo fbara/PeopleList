ad5255af2603659b90ffb6dd21a3090a212a26fd
Frank S. Bara <fbara1@yahoo.com>
2019-10-12 00:32:35 -0500

    Initial Commit

---
+++ PeopleList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PeopleList
+//
+//  Created by Frank Bara on 10/12/19.
+//  Copyright © 2019 BaraLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
